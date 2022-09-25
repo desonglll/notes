7a17cf51fcc7989e90c882331444145a0e99591a
desonglll <lindesong666@163.com>
2022-09-25 15:53:59 +0800

    auto commit

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
-echo "# notes" >> README.md
 git init
 git add .
-git commit -m "first commit"
+git commit -m "auto commit"
 git branch -M main
-git remote add origin git@github.com:desonglll/notes.git
-git push -u -f origin main
+git remote add origin git@github.com:desonglll/markdown.git
+git push -u origin main -f
